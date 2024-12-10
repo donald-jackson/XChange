25ab79c5b284cc0472a4c75b26256db9824d2010
Donald Jackson <donald@ddj.co.za>
2024-12-10 20:32:47 +0200

    [binance] add network support to deposit addresses

--- xchange-binance/src/main/java/org/knowm/xchange/binance/BinanceAuthenticated.java
+++ xchange-binance/src/main/java/org/knowm/xchange/binance/BinanceAuthenticated.java
@@ -499,6 +499,7 @@ public interface BinanceAuthenticated extends Binance {
   @Path("/sapi/v1/capital/deposit/address")
   DepositAddress depositAddress(
       @QueryParam("coin") String coin,
+      @QueryParam("network") String network,
       @QueryParam("recvWindow") Long recvWindow,
       @QueryParam("timestamp") SynchronizedValueFactory<Long> timestamp,
       @HeaderParam(X_MBX_APIKEY) String apiKey,
--- xchange-binance/src/main/java/org/knowm/xchange/binance/service/BinanceAccountServiceRaw.java
+++ xchange-binance/src/main/java/org/knowm/xchange/binance/service/BinanceAccountServiceRaw.java
@@ -7,6 +7,9 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import org.knowm.xchange.binance.BinanceAdapters;
 import org.knowm.xchange.binance.BinanceExchange;
 import org.knowm.xchange.binance.dto.BinanceException;
@@ -25,6 +28,8 @@ import org.knowm.xchange.client.ResilienceRegistries;
 import org.knowm.xchange.currency.Currency;
 
 public class BinanceAccountServiceRaw extends BinanceBaseService {
+  private List<BinanceCurrencyInfo> currencyInfos;
+  private final Lock currencyInfoLock = new ReentrantLock();
 
   public BinanceAccountServiceRaw(
       BinanceExchange exchange, ResilienceRegistries resilienceRegistries) {
@@ -95,10 +100,15 @@ public class BinanceAccountServiceRaw extends BinanceBaseService {
   }
 
   public DepositAddress requestDepositAddress(Currency currency) throws IOException {
+    return requestDepositAddressWithNetwork(currency, null);
+  }
+
+  public DepositAddress requestDepositAddressWithNetwork(Currency currency, String network) throws IOException {
     return decorateApiCall(
             () ->
                 binance.depositAddress(
                     BinanceAdapters.toSymbol(currency),
+                    network,
                     getRecvWindow(),
                     getTimestampFactory(),
                     apiKey,
@@ -213,5 +223,25 @@ public class BinanceAccountServiceRaw extends BinanceBaseService {
         .withRetry(retry("transferSubUserHistory"))
         .withRateLimiter(rateLimiter(REQUEST_WEIGHT_RATE_LIMITER))
         .call();
+  }
+
+  protected List<BinanceCurrencyInfo> getCurrencyInfoCached() throws IOException {
+    currencyInfoLock.lock();
+    try {
+      if (currencyInfos == null) {
+        currencyInfos = currencyInfos();
+      }
+      currencyInfos = currencyInfos();
+    } finally {
+      currencyInfoLock.unlock();
+    }
+
+    return currencyInfos;
+  }
+
+  protected Optional<BinanceCurrencyInfo> getCurrencyInfo(Currency currency) throws IOException {
+    return getCurrencyInfoCached().stream()
+        .filter(info -> currency.equals(info.getCurrency()))
+        .findFirst();
   }
 }
